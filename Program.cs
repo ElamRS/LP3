d5899b5e136ca8c342477334274157acfbfef205
Elam Rufino <107227033+ElamRS@users.noreply.github.com>
2023-03-01 20:07:43 -0300

    Sequência de Fibonnaci

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int a = 0, b = 1, temp;
+int n = 40;
+int flag = 0;
+
+while (flag<n) {
+    temp = a;
+    a = a + b;
+    b = temp;
+    Console.Write(a + " ");
+    flag++;
+}
+
+
