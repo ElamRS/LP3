e0b1bcd5a64e475fe55be8c067994a54e5920947
Elam Rufino <107227033+ElamRS@users.noreply.github.com>
2023-03-08 10:21:05 -0300

    Add files via upload

---
+++ Aula_03/Array_String.cs
@@ -0,0 +1,12 @@
+﻿String aulaIntro = "Introdução às Coleções";
+String aulaModelando = "Modelando a Classe Aula";
+String aulaSets = "Trabalhando com Conjuntos";
+
+string[] aulas = new string[3];
+aulas[0] = aulaIntro;
+aulas[1] = aulaModelando;
+aulas[2] = aulaSets;
+
+foreach (var aula in aulas) {
+    Console.WriteLine(aula);
+}
