ab249a0ff4d97ba1e173905c9e2238bb02e821a1
Elam Rufino <107227033+ElamRS@users.noreply.github.com>
2023-03-15 11:07:03 -0300

    Add files via upload

---
+++ P04/Introducao_List.cs
@@ -0,0 +1,9 @@
+﻿var peixes = new List<string>();
+peixes.Add("Bagre Ensaboado");
+peixes.Add("Tilápia");
+peixes.Add("Camarão");
+peixes.Add("Sardinha");
+
+foreach (var peixe in peixes) {
+    Console.Write(peixe + " ");
+}
